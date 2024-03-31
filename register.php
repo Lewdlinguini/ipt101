c9c1ab3b9b6b8dca7c0b2c016fd5288fb7b47b12
Carl <wazzy42069@gmail.com>
2024-03-31 18:19:48 +0800

    Commit

--- register.php
+++ register.php
@@ -2,55 +2,82 @@
 <html>
 <head>
     <title>Registration</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="Stylesheet.css">
+    <style>
+       
+        .container {
+            max-width: 600px; 
+            margin-top: 50px; 
+        }
+        
+        .btn-black {
+            background-color: black;
+            color: white;
+            border: none; 
+        }
+
+        .btn.btn-black:focus,
+        .btn.btn-black.focus {
+            box-shadow: none !important;
+            outline: none !important;
+        }
+    </style>
 </head>
 <body>
     <form action="register.php" method="post"> 
-        <h2>Registration</h2>
-        <?php 
-        include "db_conn.php"; 
-        
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $username = $_POST['username'];
-            $password = $_POST['password'];
-            $lastname = $_POST['lastname'];
-            $firstname = $_POST['firstname'];
-            $middlename = $_POST['middlename'];
-            $email = $_POST['email'];
+        <div class="container">
+            <h2>Registration</h2>
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username" class="form-control" placeholder="Username" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" class="form-control" placeholder="Password" required>
+            </div>
+            <div class="form-group">
+                <label for="lastname">Last Name</label>
+                <input type="text" name="lastname" id="lastname" class="form-control" placeholder="Last Name" required>
+            </div>
+            <div class="form-group">
+                <label for="firstname">First Name</label>
+                <input type="text" name="firstname" id="firstname" class="form-control" placeholder="First Name" required>
+            </div>
+            <div class="form-group">
+                <label for="middlename">Middle Name</label>
+                <input type="text" name="middlename" id="middlename" class="form-control" placeholder="Middle Name" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" class="form-control" placeholder="Email" required>
+            </div>
+            <button type="submit" class="btn btn-primary btn-black">Register</button>
+            <?php 
+            include "db_conn.php"; 
+            
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $username = $_POST['username'];
+                $password = $_POST['password'];
+                $lastname = $_POST['lastname'];
+                $firstname = $_POST['firstname'];
+                $middlename = $_POST['middlename'];
+                $email = $_POST['email'];
 
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                echo '<p class="error">Invalid email format</p>';
-            } else {
-               
-                $sql = "INSERT INTO user (username, password, Lastname, First_name, Middle_name, Email) 
-                        VALUES ('$username', '$password', '$lastname', '$firstname', '$middlename', '$email')";
-                if (mysqli_query($conn, $sql)) {
-                    echo '<p class="success">Registration successful.<a href="Loginform.php">login</a></p>';
+                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                    echo '<p class="error">Invalid email format</p>';
                 } else {
-                    echo '<p class="error">Registration failed.</p>';
+                    $sql = "INSERT INTO user (username, password, Lastname, First_name, Middle_name, Email) 
+                            VALUES ('$username', '$password', '$lastname', '$firstname', '$middlename', '$email')";
+                    if (mysqli_query($conn, $sql)) {
+                        echo '<p class="success">Registration successful. <a href="Loginform.php" class="btn btn-primary btn-black">Login</a></p>';
+                    } else {
+                        echo '<p class="error">Registration failed.</p>';
+                    }
                 }
             }
-        }
-        ?> 
-        <label>Username</label>
-        <input type="text" name="username" placeholder="Username" required>
-        <br>
-        <label>Password</label>
-        <input type="password" name="password" placeholder="Password" required>
-        <br>
-        <label>Last Name</label>
-        <input type="text" name="lastname" placeholder="Last Name" required>
-        <br>
-        <label>First Name</label>
-        <input type="text" name="firstname" placeholder="First Name" required>
-        <br>
-        <label>Middle Name</label>
-        <input type="text" name="middlename" placeholder="Middle Name" required>
-        <br>
-        <label>Email</label>
-        <input type="email" name="email" placeholder="Email" required>
-        <br>
-        <button type="submit">Register</button>
+            ?> 
+        </div>
     </form>
 </body>
 </html>
